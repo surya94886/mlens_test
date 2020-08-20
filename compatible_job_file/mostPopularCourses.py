d2003699654e1630693ff477da277c25bd614128
Surya <surya@knowledgelens.com>
2020-08-20 21:15:10 +0550

    Added compatible files

--- compatible_job_file/mostPopularCourses.py
+++
@@ -1,9 +0,0 @@
-spark.sql("""USE videoDB""")
-
-# COMMAND ----------
-
-hiveconf = dbutils.widgets.fetch('hiveconf')
-spark.sql("""SELECT courseID, COUNT(DISTINCT userID) AS uniqueUserCount FROM user_course_activity GROUP BY courseID SORT BY uniqueUserCount DESC LIMIT ${hiveconf}""".format(hiveconf=hiveconf))
-
-# COMMAND ----------
-
