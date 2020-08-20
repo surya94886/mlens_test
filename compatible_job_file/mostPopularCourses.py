22266c3274b23f21f0375769e5d14c25cda3796c
Surya <surya@knowledgelens.com>
2020-08-20 20:46:19 +0550

    Added compatible files

---
+++ compatible_job_file/mostPopularCourses.py
@@ -0,0 +1,8 @@
+spark.sql("""USE videoDB""")
+
+# COMMAND ----------
+
+spark.sql("""SELECT courseID, COUNT(DISTINCT userID) AS uniqueUserCount FROM user_course_activity GROUP BY courseID SORT BY uniqueUserCount DESC LIMIT ${hiveconf:N}""")
+
+# COMMAND ----------
+
---
+++ compatible_job_file/randomDataGenerator.py
@@ -0,0 +1,4 @@
+spark.sql("""from random import randintfor i in range(1, 100000000):    userid = randint(1,1000)    courseid = randint(10,10000)    pos = randint(0, 150) * 5    print ("\t".join([str(userid), str(courseid), str(pos)]))""")
+
+# COMMAND ----------
+
